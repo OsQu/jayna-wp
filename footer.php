54cb78104eac4b98c699e836b376c4642d33d5a4
Oskari Virtanen <oskari@flowdock.com>
2016-03-22 19:08:08 +0200

    Update contact info + some minor stuff

--- footer.php
+++ footer.php
@@ -3,42 +3,55 @@
     <div id="sponsors" class="box">
         <h3>YHTEISTYÖSSÄ</h3>
         <a href="http://www.tek.fi/" target="_blank">
-            <img src="<?php image_src("tek_2012.png"); ?>">
+            <img src="http://www.jayna.fi/wp/wp-content/uploads/2016/03/TekniikanAkateemiset120v_Logo.png">
         </a>
 
-        <a href="https://alumninet.aalto.fi/portal/Registration/Join.aspx" target="_blank">
-            <img src="<?php image_src("alumninet.png"); ?>">
+        <a href="http://new.abb.com/fi/uralle" target="_blank">
+            <img src="http://www.jayna.fi/wp/wp-content/uploads/2015/03/ABB1_rgb300_10mm.png">
         </a>
-
-        <a href="http://www.honda.fi/" target="_blank">
-            <img src="<?php image_src("honda.png"); ?>">
         </a>
 
         <a href="http://www.yit.fi/" target="_blank">
-            <img src="<?php image_src("yit.png"); ?>">
+            <img src="http://www.jayna.fi/wp/wp-content/uploads/2015/03/yit.png">
         </a>
 
-        <a href="http://www.antonanton.fi/?locale=fi" target="_blank">
-            <img src="<?php image_src("antonanton.png"); ?>">
+        <!--<a href="http://www.verkkokauppa.com" target="_blank">
+            <img src="http://www.jayna.fi/wp/wp-content/uploads/2015/03/verkkokauppacom.png">
+        </a>-->
+
+        <a href="http://www.antonanton.fi/" target="_blank">
+            <img src="http://www.jayna.fi/wp/wp-content/uploads/2015/04/AntonAnton.png">
         </a>
+
+        <a href="http://www.radissonblu.fi/hotelli-espoo" target="_blank">
+            <img src="http://www.jayna.fi/wp/wp-content/uploads/2015/04/RadissonEspooLogo.jpg">
+        </a>
+
+        <a href="http://www.honda.fi/" target="_blank">
+            <img src="http://www.jayna.fi/wp/wp-content/uploads/2015/03/honda.png">
+
+	<a href="http://www.seikkailupuistohuippu.fi/cms/fi/" target="_blank">
+            <img src="http://www.jayna.fi/wp/wp-content/uploads/2015/04/SeikkailupuistoHuippu.jpg">
+        </a>
+
     </div>
     <div id="yhteystiedot" class="box">
         <h3>YHTEYSTIEDOT</h3>
-        <p>Yleisissä Jäynäkilpailuun liittyvissä kysymyksissä tavoitat meidät parhaiten <a href="http://www.jayna.fi/kisa/index.php?page=jayna20-yhteydenotto" title="Kysy jäynästä">Kysy jäynästä -lomakkeella</a>.</p>
+        <p>Yleisissä Jäynäkilpailuun liittyvissä kysymyksissä tavoitat meidät parhaiten <a href="http://www.jayna.fi/wp/?page_id=37" title="Kysy jäynästä">Kysy jäynästä -lomakkeella</a>.</p>
         <p>Alta löydät tietyn osa-alueen vastaavien yhteystiedot.</p>
         <strong>Jäynäpromot</strong>
         <p>
-            Simo Hellgren<br>
-            simo.hellgren (at) aalto.fi
+            Rosa Haukkovaara<br>
+            rosa.haukkovaara (at) aalto.fi
         </p>
-        <strong><a href="http://www.jayna.fi/kisa/index.php?page=jayna20-yrityksille" title="Yrityksille">Yritysyhteistyö</a></strong>
+        <strong><a href="http://www.jayna.fi/wp/?page_id=17" title="Yrityksille">Yritysyhteistyö</a></strong>
         <p>
-            Hannes Kallioinen<br>
-            hannes.kallioinen (at) aalto.fi
+            Tommi Levo<br>
+            tommi.levo (at) aalto.fi
         </p>
-        <strong> <a href="http://www.jayna.fi/kisa/index.php?page=jayna20-tuomaristo" title="Tuomaristo">Ylituomaristo</a></strong>
+        <strong> <a href="http://www.jayna.fi/wp/?page_id=27" title="Tuomaristo">Ylituomaristo</a></strong>
         <p>
-            Puheenjohtaja Antti Räty<br>
+            Puheenjohtaja Santeri Kokkinen<br>
             ylituomari (at) jayna.fi
         </p>
     </div>
